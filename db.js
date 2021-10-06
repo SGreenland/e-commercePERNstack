21572e2b83b4c8c9b96e2596c68df64ed72823a2
Your Name <you@example.com>
2021-10-06 11:19:00 +0100

    changed db credentials

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     database: "e_commerce_db"
+// });
+
 const pool = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
+    user: "typlaqdiwgybpu",
+    password: "5bb82b6174f23151328fcd8602377bc06f9c7a6c041b726d8f6d53cee09237ed",
+    host: "ec2-54-155-61-133.eu-west-1.compute.amazonaws.comsslmode=require",
     port: 5432,
-    database: "e_commerce_db"
+    database: "d9fqjg6nvd8bj5"
 });
 
 module.exports = pool;
