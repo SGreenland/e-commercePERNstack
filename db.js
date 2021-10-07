3eb4f080e325396c1cd115ba9c772645ee363721
Your Name <you@example.com>
2021-10-07 12:47:41 +0100

    pool config

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Pool = require('pg').Pool;
 
 
+
 // const pool = new Pool({
 //     user: "postgres",
 //     password: "postgres",
@@ -11,6 +12,9 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     user: "typlaqdiwgybpu",
     password: "5bb82b6174f23151328fcd8602377bc06f9c7a6c041b726d8f6d53cee09237ed",
     host: "ec2-54-155-61-133.eu-west-1.compute.amazonaws.com",
