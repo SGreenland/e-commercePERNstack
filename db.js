d054a7df884266963d7077a6fbaae67bc416586d
Your Name <you@example.com>
2021-10-07 10:44:49 +0100

    remove sslmode

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "typlaqdiwgybpu",
     password: "5bb82b6174f23151328fcd8602377bc06f9c7a6c041b726d8f6d53cee09237ed",
-    host: "ec2-54-155-61-133.eu-west-1.compute.amazonaws.comsslmode=require",
+    host: "ec2-54-155-61-133.eu-west-1.compute.amazonaws.com",
     port: 5432,
     database: "d9fqjg6nvd8bj5"
 });
