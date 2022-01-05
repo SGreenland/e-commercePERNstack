fde8b74b71413e421bfcade4e9d12f5ba1f0c572
SGreenland <78597962+SGreenland@users.noreply.github.com>
2022-01-05 00:08:49 +0000

    fixing auth middleware

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   try {
     const cookie = req.cookies.token;
 
     if (jwt.verify(cookie, process.env.JWT_SECRET)) {
-      res.send("all good here");
+      return next();
     }
   } catch (err) {
     console.error(err.message);
