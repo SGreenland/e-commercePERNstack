998e59ef84e339dfa3adadb5a85186d1964d442c
SGreenland <78597962+SGreenland@users.noreply.github.com>
2022-01-05 00:41:07 +0000

    fix auth middleware

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -5,6 +5,8 @@ module.exports = async (req, res, next) => {
   try {
     const cookie = req.cookies.token;
 
+    console.log(cookie);
+
     if (jwt.verify(cookie, process.env.JWT_SECRET)) {
       next();
     }
